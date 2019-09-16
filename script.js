be5be3856a44098a97da5b7577bd89757b364047
gelissa <gelissaleveille@yahoo.com>
2019-09-15 23:28:33 -0400

    fixed button and added a loop

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-// create an onclick that calls the buttons and everything else
-$("#submit").on("click", function(){
-
 // an array of movies that the submit button pushes into
 // the variable is called topics
 
 var topics = ["Inception", "IT", "Annihilation", "Titanic", "The Dark Knight", "Spiderman", "Mission Impossible", "Black Panther", "Endgame"];
+
+function buttons(){
+    for(var i = 0; i < topics.length; i++){
+        $(".button-items").append("<button>" + topics[i] + "</button>")
+    }
+}
+
+buttons();
+
+
+// create an onclick that calls the buttons and everything else
+$(".main-content").on("click", function(){
+
+// var movieTopics = $(this).attr
 
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
 topics + "&api_key=Oek1NkJPgKEtBktiKcsXU9MBfSUNjAG1&limit=10";
@@ -31,7 +42,11 @@ $.ajax({
         // the img tag for the gifs
         var movieImage = $("<img>");
 
+        movieImage.attr("src", results[i].images.fixed_height_url);
+        movieImage.prepend(p);
+        movieDiv.prepend(movieImage);
 
+        $("main-content").prepend(movieDiv);
     }
 })
 });

311ae9290d5f3e1fdc52edbbcc88c8be84d7a89f
gelissa <gelissaleveille@yahoo.com>
2019-09-16 14:33:52 -0400

    fixed movie buttons; working on input button

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 // an array of movies that the submit button pushes into
 // the variable is called topics
 
-var topics = ["Inception", "IT", "Annihilation", "Titanic", "The Dark Knight", "Spiderman", "Mission Impossible", "Black Panther", "Endgame"];
+var topics = ["Inception", "IT", "Annihilation", "Titanic", "The+Dark+Knight", "Spiderman", "Mission Impossible", "Black Panther", "Endgame"];
 
 function buttons(){
     for(var i = 0; i < topics.length; i++){
-        $(".button-items").append("<button>" + topics[i] + "</button>")
+        var btn  = $("<button>");
+        // give the  button a class 
+        btn.addClass("movie-button");
+        //then target the button with the class
+        btn.attr("data-movie", topics[i]);
+        // var movieName = 
+        // console.log(topics[i]);
+        btn.text(topics[i]);
+        $(".button-items").append(btn); 
     }
-}
+};
 
 buttons();
 
 
 // create an onclick that calls the buttons and everything else
-$(".main-content").on("click", function(){
+$(".movie-button").on("click", function(){
 
-// var movieTopics = $(this).attr
+console.log("Clicked on one")
+// allows me to grab the info form the data-movie which should align with it's own value (it's name)
+var movieTopics = $(this).attr("data-movie");
 
+// starts the GIPHY call
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-topics + "&api_key=Oek1NkJPgKEtBktiKcsXU9MBfSUNjAG1&limit=10";
+movieTopics + "&api_key=Oek1NkJPgKEtBktiKcsXU9MBfSUNjAG1&limit=10";
+
+console.log(movieTopics);
+console.log(queryURL);
+
+// button for the submit input
+$("#submit").on("click", function(event){
+    // stop submit button from doing whatever it does
+    event.preventDefault();
+    // get a movie from the user
+    var userMovie = $("#add-item").val().trim();
+    // push that movie to the topics array
+    console.log(userMovie);
+    // console.log($("topics").push(userMovie))
+    // run the button function to add the item. 
+    // buttons();
+});
+
 
 // getting the api content from giphy
 $.ajax({
@@ -42,11 +70,11 @@ $.ajax({
         // the img tag for the gifs
         var movieImage = $("<img>");
 
-        movieImage.attr("src", results[i].images.fixed_height_url);
+        movieImage.attr("src", results[i].images.fixed_height.url);
         movieImage.prepend(p);
         movieDiv.prepend(movieImage);
 
-        $("main-content").prepend(movieDiv);
+        $(".main-content").prepend(movieDiv);
     }
 })
 });
